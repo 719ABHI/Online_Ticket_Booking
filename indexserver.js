0d124de4d4db55bfc7b99e36cbb0cc5312c84f05
719ABHI <131292206+719ABHI@users.noreply.github.com>
2025-05-28 10:59:49 +0530

    Update indexserver.js

--- indexserver.js
+++ indexserver.js
@@ -586,7 +586,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 // MongoDB connection (adjust your URI accordingly)
-const dbURI = 'mongodb://host.docker.internal:27017/yourdbname'; // or your real connection string
+const dbURI = 'mongodb://localhost:27017/trainbooking'; // or your real connection string
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log('MongoDB connection error:', err));
@@ -776,7 +776,7 @@ app.get('/api/hello', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
