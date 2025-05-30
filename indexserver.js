f4bd6f4c4bf6d97a4b42247fc1e545ec20d84615
Abhis <abhishekangadi2004@gmail.com>
2025-05-30 16:45:09 +0530

    latest updated code

--- indexserver.js
+++ indexserver.js
@@ -586,8 +586,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 // MongoDB connection (adjust your URI accordingly)
-const dbURI = 'mongodb://host.docker.internal:27017/trainbooking'; // or your real connection strin
-//const dbURI = 'mongodb://localhost:27017/trainbooking'; // or your real connection string
+//const dbURI = 'mongodb://host.docker.internal:27017/trainbooking'; // or your real connection strin
+const dbURI = 'mongodb://localhost:27017/trainbooking'; // or your real connection string
 //>>>>>>> 0d124de4d4db55bfc7b99e36cbb0cc5312c84f05
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
